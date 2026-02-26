6718d194fd47fb62e7e44b59a66e1ea103de89db
Dinesh Solanki <dineshsolanki2799@gmail.com>
2026-02-26 21:06:24 +0530

    added screenshot capture file

---
+++ Playwrite/2.screenshot-capture.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.google.com")
+    page.screenshot(path="screenshot.png")
+    page.wait_for_timeout(5000)
+    page.close()
