4b499f702aae962349c61bfac203a824588712c0
Dinesh Solanki <dineshsolanki2799@gmail.com>
2026-02-28 11:54:45 +0530

    added my first post request

--- FastAPI/simple-message-get.py
+++ FastAPI/simple-message-get.py
@@ -7,3 +7,11 @@ app = FastAPI() # Fast api instance is "app"
 @app.get("/") # decorator
 async def read_root(): # async is used for handling multiple requests
     return {"message": "Welcome to my FastAPI"}
+
+@app.get("/posts")
+async def read_posts():
+    return {"message": "This is your post"}
+
+@app.post("/postrequest")
+async def create_post():
+    return {"message": "Post created"}
